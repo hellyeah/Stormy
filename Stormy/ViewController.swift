8723fbde5844df96e8eb53f220b2ad71918c2e5f
gailees <gailees@umich.edu>
2014-12-29 05:44:22 -0500

    a storm is brewin

---
+++ Stormy/ViewController.swift
@@ -0,0 +1,29 @@
+//
+//  ViewController.swift
+//  Stormy
+//
+//  Created by David Fontenot on 12/29/14.
+//  Copyright (c) 2014 HackMatch. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    
+    //access controls
+    //3 access levels
+    private let apiKey = "459b10c1c58316fabe5154a4d819de2f"
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
